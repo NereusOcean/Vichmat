5e71e231470b51f0bae8578a362ad3502a2eac3d
Nikita <46265091+NereusOcean@users.noreply.github.com>
2021-01-23 15:16:20 +0300

    Update main.cpp

--- Task1/main.cpp
+++ Task1/main.cpp
@@ -1,10 +1,9 @@
 #include "back/Triangle.h"
 #include "math.h"
 int main(int argc, char * argv []) {
-    double x = 1.5;
-    double y = 1.5;
-    double z = 2.4494897427832;
+    double length = 3;
+    double height = 2.4494897427832;
     double mass = 2;
-    Start(argc,argv,z,x,y,mass);
+    Start(argc,argv,height,length,mass);
     return 0;
 }
