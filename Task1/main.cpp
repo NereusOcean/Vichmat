3745e81d2e54100dbd95ba405118dfdc33026b38
NereusOcean <niki_5555@mail.ru>
2021-01-28 04:57:52 +0300

    dd

--- Task1/main.cpp
+++ Task1/main.cpp
@@ -1,8 +1,8 @@
 #include "back/Triangle.h"
 #include "math.h"
 int main(int argc, char * argv []) {
-    double length = 6;
-    double height = 0.5;
+    double length = 0.5;
+    double height = 2.8;
     double mass = 2;
     Start(argc,argv,height,length,mass);
     return 0;
